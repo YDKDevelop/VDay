13acc0ae3fa67ede3ba45176cdbb99ae4a45ec95
Kenny <23202039+YDKDevelop@users.noreply.github.com>
2025-02-12 14:30:06 -0500

    feb12

--- VDAY_1/src/App.tsx
+++ VDAY_1/src/App.tsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
 
-function Website() 
-{
 
+
+function Button()
+{
+    return (
+        <button>
+            <h1>Click Me</h1>
+
+
+        </button>
+    );
+}
+
+export default function Website() 
+{
+    return(
+        <div>
+            <h1>Happy Valentine's Day</h1>
+            <Button />
+
+        </div>
+    );
 
 }
 
-export default Website;
+
